8e7223bdc7b09a90771d8274943d09f16a94f896
Ann-Sofi Lantz <48385990+ansolantz@users.noreply.github.com>
2019-05-23 20:00:00 +0200

    New user route

--- routes/user.js
+++ routes/user.js
@@ -2,47 +2,66 @@ const express = require('express');
 const createError = require('http-errors');
 const router = express.Router();
 const mongoose = require('mongoose');
+
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
-
-
 // HELPER FUNCTIONS
-const {
-  isLoggedIn,
-  isNotLoggedIn,
-  validationLoggin,
-} = require('../helpers/middlewares');
+const { validationLoggin } = require('../helpers/middlewares');
 
+router.post('/signup', validationLoggin(),
+  async (req, res, next) => {
+    const { username, password } = req.body;
 
+    try {
+      const user = await User.findOne({ username }, 'username');
+      if (user) {
+        return next(createError(422));
+      } else {
+        const salt = bcrypt.genSaltSync(10);
+        const hashPass = bcrypt.hashSync(password, salt);
+        const newUser = await User.create({ username, password: hashPass });
 
-//router.put('/update/:id', isLoggedIn(), (req, res, next) => {
-router.put('/update/', isLoggedIn(), (req, res, next) => {
-
-  //const { displayName, email, url, age, weight, height } = req.body;
-  const userId = req.session.currentUser._id
-  console.log("inside and user is", userId);
-
-  User.findByIdAndUpdate(userId, req.body)
-
-    //User.findByIdAndUpdate(req.params.id, { displayName, email })
-    .then(async () => {
-      res.json({ message: `User with ${userId} is updated.` });
-      console.log("did find user");
-      try {
-        const user = await User.findOne({ _id: userId });
-        console.log("hi there");
-        console.log(user)
-        req.session.currentUser = user;
-      } catch (error) {
-        console.log("this is bad");
-        next(error);
+        res.status(200).json(newUser);
       }
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
+router.post('/logout', (req, res, next) => {
+  return res.status(204).send();
+});
+
+
+//  GET    '/me'
+router.get('/me/:id', async (req, res, next) => {
+
+  if (req.params.id) {
+    const user = await User.findOne({ _id: req.params.id });
+    res.json(user);
+  } else {
+    res.status(500).send()
+  }
+});
+
+
+//  PUT    '/update'
+router.put('/update/', (req, res, next) => {
+
+  const { _id } = req.body;
+  let dataToUppdate = { ...req.body };
+  delete dataToUppdate._id;
+
+  console.log("inside and user is", _id);
+
+  User.findByIdAndUpdate({ _id }, dataToUppdate)
+
+    .then(() => {
+      res.json({ message: `User with ${_id} is updated.` })
     })
-    .catch(err => {
-      console.log("error finding user", err);
-      res.json(err);
-    })
+    .catch(err => { res.json(err) })
 });
 
 module.exports = router;
