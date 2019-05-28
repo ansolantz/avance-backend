0a51331a746683a561ee5544004a28ec7a69b9b9
Ann-Sofi Lantz <Anso@Anso.local>
2019-05-28 11:42:43 +0200

    remove cors

--- app.js
+++ app.js
@@ -31,12 +31,12 @@ mongoose.set('useFindAndModify', false);
 
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: [process.env.PUBLIC_DOMAIN],
-  }),
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: [process.env.PUBLIC_DOMAIN],
+//   }),
+// );
 
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
